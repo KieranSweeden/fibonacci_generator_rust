44449f21211b2f302fb26ab02ce28eaed66068d5
kieran <kieransweeden@gmail.com>
2023-04-29 17:14:54 +0100

    Add prompt for user input to dictate fibonacci index to generate to

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    intro();
+
+    loop {
+        let index = prompt_user_for_index();
+
+        println!("{} was the fibonacci index provided", index);
+    }
+}
+
+fn prompt_user_for_index() -> i32 {
+    loop {
+        println!("Please provide the index of the fibonacci number to generate:");
+
+        let mut input = String::new();
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Input was invalid");
+
+        let input = input.trim().parse::<i32>();
+
+        match input {
+            Result::Ok(number) => break number,
+            Result::Err(_) => {
+                println!("Input provided was not a valid number.");
+                continue;
+            }
+        }
+    }
+}
+
+fn intro() {
+    println!("================");
+    println!("Welcome to the fibonacci generator!");
+    println!("================");
 }
